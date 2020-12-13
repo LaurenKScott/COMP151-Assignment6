7bc946dddf97240bca32b497ccf43613bade90b2
LaurenKScott <lkscottcom@gmail.com>
2020-12-12 22:39:23 -0500

    Update adventure.py: clean play_game function

--- adventure.py
+++ adventure.py
@@ -12,36 +12,38 @@ import iteminventory as ii
 import gmap as mp
 import gameparse as pr 
 
-#Initialize inventory
+#Initialize player's empty inventory (see iteminventory.py)
 player_inv = ii.player_inv
 #initialize island map (see map.py for tile info)
 island = mp.game_map
-#Initialize parser
+#Initialize parser (see gameparse.py for command info)
 parser = pr.cmdp
 
+#set win condition
 def win_condition():
     if island.get_location() == mp.tile10 and island.get_location().item is None:
         print("you win")
         return True
     return False
 
+# gameplay function, takes string user_command and parses input. then translates into executable command
 def play_game():
+    user_command = input("Enter a command: ")
+    parser.parse(user_command)
+    print()
+    parser.command_choose()
+    print()
+   
+
+def main():
+    # start of game, print initial tile description
     print("THE LIGHTHOUSE")
     print()
     print(island.get_location().get_description())
     while parser.continue_game() and not win_condition():
-        user_command = input("Enter a command: ")
-        parser.parse(user_command)
-        parser.command_choose()
-        #print newline between commands
-        print()
+        play_game()
     else:
         print("Goodbye.")
-    
-#set win condition
-def main():
-
-    play_game()
-
-    
-main()
+        
+if __name__ == '__main__':
+    main()
